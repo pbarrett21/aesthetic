a172e1565ac49d40618d8efe2f9fe07e4d09f278
pbarrett21 <paulg.barrett4@gmail.com>
2023-09-06 17:39:27 -0400

    wip

--- src/components/Extraordinary.tsx
+++ src/components/Extraordinary.tsx
@@ -1,28 +1,30 @@
+import Text from "../text"
+
 const Extraordinary: React.FC = () => {
     return (
         <>
             <div className='extraordinary'>
                 <h1>Unveiling the Extraordinary</h1>
                 <div className="bottom-panel">
-                    <div>
+                    <div className="left-side">
                         <div>
-                            <h2>h2</h2>
-                            <p>p</p>
+                            <h2>Design Expertise</h2>
+                            <p>{Text.designExpertise}</p>
                         </div>
                         <div>
-                            <h2>h2</h2>
-                            <p>p</p>
+                            <h2>Innovative Solutions</h2>
+                            <p>{Text.innovative}</p>
                         </div>
                     </div>
                     <img src='src/assets/colorful.jpeg' />
-                    <div>
+                    <div className="right-side">
                         <div>
-                            <h2>h2</h2>
-                            <p>p</p>
+                            <h2>Client-Centric Approach</h2>
+                            <p>{Text.clientCentric}</p>
                         </div>
                         <div>
-                            <h2>h2</h2>
-                            <p>p</p>
+                            <h2>Tailored Budgeting</h2>
+                            <p>{Text.tailored}</p>
                         </div>
                     </div>
                 </div>
--- src/text.ts
+++ src/text.ts
@@ -3,8 +3,11 @@ const Text = {
         'We believe that a well designed space can have a profound impact on your lifestyle and well-being. Our skilled interior designers are dedicated to turning your vision into a reality.',
     commercialDesign: 'Impress clients and boost productivity with our innovative commercial design solutions',
     residentialDesign: 'From living rooms to bedrooms, kitchens to bathrooms, we\'ll curate designs that resonate with your style',
-    interiorStyling: 'Our team will breathe new life into your existing decor, making subtle yet impactful changes'
-    
+    interiorStyling: 'Our team will breathe new life into your existing decor, making subtle yet impactful changes',
+    designExpertise: 'With a diverse portfolio of successful projects, we possess the expertise to handle a wide range of design challenges',
+    innovative: 'We\'re committed to staying ahead of design trends and technology. Our desings combine timeless elegance',
+    clientCentric: 'Your satisfaction is our priority. We collaborate closely with you, ensuring your preferences and needs are integrated seamlessly',
+    tailored: 'We understand that every project comes with a unique budget. Our transparent pricing ensures that you receive the best'
 }
 
 export default Text
