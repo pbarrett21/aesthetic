f64913ab37aa8146c58f3d81f5088186e6e01afb
pbarrett21 <paulg.barrett4@gmail.com>
2023-09-13 17:34:38 -0400

    add window size hook

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,28 +1,24 @@
-import Action from './Action'
-import Brag from './Brag'
+import Action from "./Action";
+import Brag from "./Brag";
+import useWindowDimensions from "../helpers/useWindowDimensions";
 
 const Landing: React.FC = () => {
-    return (
-        <>
-            <div className='landingPage'>
-                <div className='img1-container'>
-                    <img
-                        className='img1'
-                        src='/room1.jpeg'
-                        alt='modern living room'
-                    />
-                </div>
-                <Action />
-                <Brag />
-                <img
-                    className='img2'
-                    src='/room2.jpeg'
-                    alt='modern house'
-                />
-            </div>
-            <hr/>
-        </>
-    )
-}
+  const { width, height } = useWindowDimensions();
+  console.log(width, height);
 
-export default Landing
+  return (
+    <>
+      <div className="landingPage">
+        <div className="img1-container">
+          <img className="img1" src="/room1.jpeg" alt="modern living room" />
+        </div>
+        <Action />
+        <Brag />
+        <img className="img2" src="/room2.jpeg" alt="modern house" />
+      </div>
+      <hr />
+    </>
+  );
+};
+
+export default Landing;
