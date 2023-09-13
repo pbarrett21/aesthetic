920185114a71a762f4d217fd21ad2e0f1a46d741
pbarrett21 <paulg.barrett4@gmail.com>
2023-09-13 11:17:17 -0400

    modify config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+// use this on Mac
+// export default defineConfig({
+//   plugins: [react()]
+// })
+
+// use this on windows to fix hmr
+// uses more cpu
+// https://vitejs.dev/config/server-options.html#server-watch
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true
+    }
+  }
 })
