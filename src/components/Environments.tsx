4ccc8488b879a65344970ca52cc4151f187c9e1b
pbarrett21 <paulg.barrett4@gmail.com>
2023-09-20 23:40:50 -0400

    add responsive changes

--- src/components/Environments.tsx
+++ src/components/Environments.tsx
@@ -1,31 +1,62 @@
-import Text from "../text"
+import Text from "../text";
+import useWindowDimensions from "../helpers/useWindowDimensions";
 
 const Environments: React.FC = () => {
-    return (
-        <>
-            <div className="environments">
-                <h1>Creating Environments Tailored to You</h1>
-                <div className="env-section">
-                    <h2>Commercial Design</h2>
-                    <p>{Text.commercialDesign}</p>
-                </div>
-                <div className="environments-accent">
-                    <div>
-                        <h2>Residential Design</h2>
-                        <p>{Text.residentialDesign}</p>
-                    </div>
-                    <div>
-                        <img src="/rotate.jpeg"/>
-                    </div>
-                </div>
-                <div className="env-section">
-                    <h2>Interior Styling</h2>
-                    <p>{Text.interiorStyling}</p>
-                </div>
-            </div>
-            <hr/>
-        </>
-    )
-}
+    const { width, height } = useWindowDimensions();
 
-export default Environments
+    if (width >= 670) {
+        return (
+            <>
+                <div className='environments'>
+                    <h1>Creating Environments Tailored to You</h1>
+                    <div className='env-section'>
+                        <h2>Commercial Design</h2>
+                        <p>{Text.commercialDesign}</p>
+                    </div>
+                    <div className='environments-accent'>
+                        <div>
+                            <h2>Residential Design</h2>
+                            <p>{Text.residentialDesign}</p>
+                        </div>
+                        <div>
+                            <img src='/rotate.jpeg' />
+                        </div>
+                    </div>
+                    <div className='env-section'>
+                        <h2>Interior Styling</h2>
+                        <p>{Text.interiorStyling}</p>
+                    </div>
+                </div>
+                <hr />
+            </>
+        );
+    } else {
+        return (
+            <>
+                <div className='environments mobile'>
+                    <h1>Creating Environments Tailored to You</h1>
+                    <div className='env-section'>
+                        <h2>Commercial Design</h2>
+                        <p>{Text.commercialDesign}</p>
+                    </div>
+                    <div className='environments-accent'>
+                        <div>
+                            <h2>Residential Design</h2>
+                            <p>{Text.residentialDesign}</p>
+                        </div>
+                        <div>
+                            <img className='mobile-rotate' src='/rotate.jpeg' />
+                        </div>
+                    </div>
+                    <div className='env-section'>
+                        <h2>Interior Styling</h2>
+                        <p>{Text.interiorStyling}</p>
+                    </div>
+                </div>
+                <hr />
+            </>
+        );
+    }
+};
+
+export default Environments;
