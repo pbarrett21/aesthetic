876b26c10d2413312f6ee0123cdc0ba63b45f4e7
pbarrett21 <paulg.barrett4@gmail.com>
2023-09-21 23:06:47 -0400

    finish responsive first draft

--- src/components/Transform.tsx
+++ src/components/Transform.tsx
@@ -1,43 +1,85 @@
-import Arrow from '../icons/Arrow'
-import FooterIcon from '../icons/FooterIcon'
+import Arrow from "../icons/Arrow";
+import FooterIcon from "../icons/FooterIcon";
+import useWindowDimensions from "../helpers/useWindowDimensions";
 
 const Transform: React.FC = () => {
-    return (
-        <>
-            <div className='action-call'>
-                <h1>Ready to transform your space?</h1>
-                <div className='big-arrow'>
-                    <Arrow right />
-                </div>
-            </div>
-            <nav className='footer'>
-                <a href='/'>Aesthetic</a>
-                <ul className='footerNav'>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Services</li>
-                    <li>Projects</li>
-                    <li>Contact</li>
-                </ul>
-                <ul className='footerLinks'>
-                    <li>
-                        <FooterIcon facebook />
-                    </li>
-                    <li>
-                        <a href='https://www.linkedin.com/in/pbarrett21/'>
-                            <FooterIcon linkedin />
-                        </a>
-                    </li>
-                    <li>
-                        <FooterIcon twitter />
-                    </li>
-                    <li>
-                        <FooterIcon instagram />
-                    </li>
-                </ul>
-            </nav>
-        </>
-    )
-}
+    const { width } = useWindowDimensions();
 
-export default Transform
+    if (width >= 670) {
+        return (
+            <>
+                <div className='action-call'>
+                    <h1>Ready to transform your space?</h1>
+                    <div className='big-arrow'>
+                        <Arrow right />
+                    </div>
+                </div>
+                <nav className='footer'>
+                    <a href='/'>Aesthetic</a>
+                    <ul className='footerNav'>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Services</li>
+                        <li>Projects</li>
+                        <li>Contact</li>
+                    </ul>
+                    <ul className='footerLinks'>
+                        <li>
+                            <FooterIcon facebook />
+                        </li>
+                        <li>
+                            <a href='https://www.linkedin.com/in/pbarrett21/'>
+                                <FooterIcon linkedin />
+                            </a>
+                        </li>
+                        <li>
+                            <FooterIcon twitter />
+                        </li>
+                        <li>
+                            <FooterIcon instagram />
+                        </li>
+                    </ul>
+                </nav>
+            </>
+        );
+    } else {
+        return (
+            <div className='call-mobile'>
+                <div className='action-call mobile'>
+                    <h1>Ready to transform your space?</h1>
+                    <div className='big-arrow'>
+                        <Arrow right />
+                    </div>
+                </div>
+                <nav className='footer'>
+                    <a href='/'>Aesthetic</a>
+                    <ul className='footerNav'>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Services</li>
+                        <li>Projects</li>
+                        <li>Contact</li>
+                    </ul>
+                    <ul className='footerLinks'>
+                        <li>
+                            <FooterIcon facebook />
+                        </li>
+                        <li>
+                            <a href='https://www.linkedin.com/in/pbarrett21/'>
+                                <FooterIcon linkedin />
+                            </a>
+                        </li>
+                        <li>
+                            <FooterIcon twitter />
+                        </li>
+                        <li>
+                            <FooterIcon instagram />
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+};
+
+export default Transform;
